665f4bc6de009b389b157202ed8562f99fd1051e
mskh1998 <79457414+mskh1998@users.noreply.github.com>
2021-02-28 16:06:25 +0330

    add errors

--- assets/js/login_recovery/recovery_email_verify.js
+++ assets/js/login_recovery/recovery_email_verify.js
@@ -1,37 +1,47 @@
-$(document).ready(function () {
-    var min = $(".min").text();
-    var sec = $(".sec").text();
-    var timer = setInterval(function () {
-        sec--;
-        if (sec < 0) {
+// timer
+var min = $(".min").text();
+var sec = $(".sec").text();
+var timer = setInterval(function () {
+    sec--;
+    if (sec < 0) {
 
-            sec = 59;
-            min--;
+        sec = 59;
+        min--;
 
-        }
-
-
-            $(".min").text(min);
-            $(".sec").text(sec);
-
-            if (min == 0 && sec == 0) {
-
-                clearInterval(timer);
-    
-            }
+    }
 
 
-    }, 1000)
+    $(".min").text(min);
+    $(".sec").text(sec);
 
-    $(".btn").click(function (e) {
+    if (min == 0 && sec == 0) {
 
-        if ($(".e_input").val() == "") {
+        clearInterval(timer);
+
+    }
 
 
-            $(".error_box").text(".جای خالی را پر کنید");
-            e.preventDefault();
-        }
+}, 1000);
 
-    });
 
+// errors
+$('.verify').click(function (e) {
+
+    let inp_email = $('#verify').val();
+
+    if (inp_email == "") {
+
+        $('.error_box').fadeIn();
+        $('.error').addClass('warning_error ');
+        $('.error .text_error').text('لطفا کد تایید را وارد کنید !');
+        e.preventDefault();
+
+    }
 });
+
+
+$('.fa-times').click(function () {
+
+    $('.error_box').fadeOut();
+    
+})
