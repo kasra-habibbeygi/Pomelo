46b60ba79d7212b49eb89cbf9c5e13eec94650cb
mskh1998 <79457414+mskh1998@users.noreply.github.com>
2021-03-04 09:52:23 +0330

    debug

--- assets/js/dashboard/dashboard.js
+++ assets/js/dashboard/dashboard.js
@@ -1,16 +1,30 @@
 $(".listIcon").click(function () {
 
+    let title = $(this);
+    let dropDown = $(this).children(".dropDown");
     $(".listIcon").children(".dropDown").css("display", "none");
+    $(".listIcon").removeClass("dropDownFirst");
+    $(".listIcon").removeClass("dropDownLast");
+
+    if (title.parent().attr("id") == "leftBox") {
+
+        title.addClass("dropDownFirst");
+
+    } else {
+
+        title.addClass("dropDownLast");
+
+    }
     $(this).children(".dropDown").toggle();
-    
+    $(window).click(function (e) {
+
+
+        if( title.attr("class") != $(e.target).attr("class") && dropDown.attr("class") != $(e.target).attr("class") ){
+
+            $(title).children(".dropDown").css("display", "none");
+        }
+
+    });  
+
 
 });
-$(".main").click(function () {
-
-    alert($(".dropDown").css("display"));
-    if ($(".dropDown").css("display") != "none") {
-        
-        $(".dropDown").css("display" , "none");
-        
-    }
-});  
