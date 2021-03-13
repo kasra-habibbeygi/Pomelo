af42ed47ba460989483a69afe27b30517e519eec
nadie-najm <nadi.codes@gmail.com>
2021-03-13 20:08:02 +0330

    edit js - open and close side bar

--- backend/tempate/assets/js/dashboard/dashboard.js
+++ backend/tempate/assets/js/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 $(".nav_list").click(function () {
-    
+
     $(".drop-down").hide();
     $(this).children(".drop-down").show();
 
@@ -7,10 +7,43 @@ $(".nav_list").click(function () {
 
 //dashbord js side bar menu
 
-$('.articles_btn').click(function(){
+$('.down_btn').click(function () {
     $('#article_menu').slideToggle();
+
 });
 
-$('.product_btn').click(function(){
-    $('#product_menu').slideToggle();
+//close menu
+$('.fa-chevron-double-right').click(function () {
+    $('.text_menu , .down_btn , .side_bar_menu ul p , .img_of_logo , .name_of_page').hide();
+    $('.main .dashboard .right_box').css({
+        'overflow-y': 'auto',
+        'width': '60px',
+    });
+    $( this ).replaceWith( "<i class='fad fa-chevron-double-left'></i>" );
+    $(this).css({
+        'font-size': ' 1.2rem',
+        'margin-right': ' 0px'
+    });
+    $('.main .dashboard .logo').css('width', '60px');
+    $('.nav_bar , .left_box').css('width', '96.1%');
+    
 });
+
+//open menu
+
+$('.fa-chevron-double-left').click(function () {
+    $('.text_menu , .down_btn , .side_bar_menu ul p , .img_of_logo , .name_of_page').show();
+    $('.main .dashboard .right_box').css({
+        'overflow-y': 'scull',
+        'width': '15%',
+    });
+    $( this ).replaceWith( "<i class='fad fa-chevron-double-right'></i>" );
+    $(this).css({
+        'font-size': ' 1.5rem',
+        'margin-right': ' 20px'
+    });
+    $('.main .dashboard .logo').css('width', '15%');
+    $('.nav_bar , .left_box').css('width', '85%');
+    
+});
+
