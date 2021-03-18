82c7bc471270aaec5618f6d0be4571475b5d888e
mskh1998 <79457414+mskh1998@users.noreply.github.com>
2021-03-18 17:32:19 +0330

    nav js problem

--- backend/tempate/assets/js/dashboard/dashboard.js
+++ backend/tempate/assets/js/dashboard/dashboard.js
@@ -1,16 +1,31 @@
+//nav bar drop down box
 $(".nav_list").click(function () {
-    
+
     $(".drop-down").hide();
     $(this).children(".drop-down").show();
 
+    // $(window).click(function (e) {
+
+    //     // if (e.target.className == "n_section") {
+    //         e.stopPropagation();
+    //         alert(e.target.className);
+    //     // }
+    // });
+    
+});
+$(".envelope_show").click(function(){
+
+    $(".envelope_show:first-child").hide();
+    $(".envelope_show:nth-child(2)").show();
+    
 });
 
 //dashbord js side bar menu
 
-$('.articles_btn').click(function(){
+$('.articles_btn').click(function () {
     $('#article_menu').slideToggle();
 });
 
-$('.product_btn').click(function(){
+$('.product_btn').click(function () {
     $('#product_menu').slideToggle();
 });
