20bf15add5f46ae39cbf9a7fc92237e01439644d
nadie-najm <nadi.codes@gmail.com>
2021-04-03 14:47:43 +0430

    dashbord side abr

--- backend/tempate/assets/js/dashboard/dashboard.js
+++ backend/tempate/assets/js/dashboard/dashboard.js
@@ -26,12 +26,14 @@ for(let j = 0 ; j < $('.submenu_btn').length ; j++){
         if($(this).next().css('display') === 'none'){
 
             $('.submenu_btn').next().slideUp();
+            $('.submenu_icon').attr('class' , 'fal fa-chevron-left submenu_icon');
             $(this).next().slideDown();
+            $('.submenu_icon').eq(j).attr('class' , 'fal fa-chevron-down submenu_icon');
 
         }else {
 
             $(this).next().slideUp();
-
+            $('.submenu_icon').eq(j).attr('class' , 'fal fa-chevron-left submenu_icon');
         }
 
     });
@@ -40,39 +42,80 @@ for(let j = 0 ; j < $('.submenu_btn').length ; j++){
 
 //close menu
 
+
 let o_c = 1;
+
+
 $('.open_close').click(function () {
 
     if (o_c === 1) {
-        $('.text_menu , .down_btn , .side_bar_menu ul p , .img_of_logo , .name_of_page').hide();
+
+        $('.text_menu , .submenu_icon , .side_bar_menu ul p , .img_of_logo , .name_of_page , .under_menu li').hide();
+
         $('.main .dashboard .right_box').css({
             'overflow-y': 'auto',
             'width': '60px',
         });
+
         $(this).attr('class', 'fad fa-chevron-double-left open_close');
-        $(this).css({
-            'font-size': ' 1.2rem',
-            'margin-right': ' 0px'
-        });
+
+        $(this).css({ 'margin-right': ' 0px'});
+
         $('.main .dashboard .logo').css('width', '60px');
+
         $('.nav_bar , .left_box').css('width', '96.1%');
+        
         o_c = 0;
-    } else {
-        $('.text_menu , .down_btn , .side_bar_menu ul p , .img_of_logo , .name_of_page').show();
+
+        for(let n = 0 ; n < $('.icon_menu').length ; n++){
+            $('.icon_menu').eq(n).mouseover(function(){
+                $(this).next().css('display' , 'block');
+                
+            });
+        }
+
+    } 
+    
+    else {
+        
+        $('.text_menu , .submenu_icon , .side_bar_menu ul p , .img_of_logo , .name_of_page , .under_menu li').show();
+
         $('.main .dashboard .right_box').css({
             'overflow-y': 'scrull',
             'width': '15%',
         });
+
         $(this).attr('class', 'fad fa-chevron-double-right open_close');
-        $(this).css({
-            'font-size': ' 1.5rem',
-            'margin-right': ' 20px'
-        });
+
+        $(this).css({ 'margin-right': ' 20px' });
+
         $('.main .dashboard .logo').css('width', '15%');
+
         $('.nav_bar , .left_box').css('width', '85%');
+        
         o_c = 1;
     }
 
 });
+
+if($(window).width() <= '768'){
+
+    $('.text_menu , .down_btn , .side_bar_menu ul p , .img_of_logo , .name_of_page').hide();
+
+    $('.main .dashboard .right_box').css({
+        'overflow-y': 'auto',
+        'width': '60px',
+    });
+
+    $(this).attr('class', 'fad fa-chevron-double-left open_close');
+
+    $(this).css({ 'margin-right': ' 0px'});
+
+    $('.main .dashboard .logo').css('width', '60px');
+
+    $('.nav_bar , .left_box').css('width', '96.1%');
+
+    o_c = 0;
+}
 
 //open menu
