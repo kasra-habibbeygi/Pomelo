bb08882e58adb8a98a813ba1837756e000805816
mskh1998 <79457414+mskh1998@users.noreply.github.com>
2021-04-07 11:56:51 +0430

    empty

---
+++ backend/tempate/assets/js/profile/profile.js
@@ -0,0 +1,21 @@
+//upload picture
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
+});
+//error box
+$('.fa-times').click(function () {
+
+    $('.error_box').fadeOut();
+
+})
